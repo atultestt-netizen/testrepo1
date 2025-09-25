e59e5bc1036370b264d2b7cde88ffa79a0b8a1e7
atultestt-netizen <atultestt@gmail.com>
2025-09-25 14:32:44 -0400

    Create test.ts

---
+++ test.ts
@@ -0,0 +1,19 @@
+const parsedHeadline = cachedHeadline
+      ? safeJsonParse<{ headline: string; daily_report: any }>(cachedHeadline)
+      : null;
+    // Check if the headline is cached
+    if (parsedHeadline) {
+      newHeadline = parsedHeadline.headline;
+      daily_report = parsedHeadline.daily_report;
+    } else {
+      // cache miss OR invalid JSON
+      newHeadline = await generateSimpleAIHeadline(user_profile);
+      daily_report = await getTodaysReportForUser(user_profile._id);
+
+      await redisClient?.set(
+        hashKey,
+        JSON.stringify({ headline: newHeadline, daily_report }),
+        "EX",
+        60 * 60 * HEADLINE_TTL_HOURS,
+      );
+    }
